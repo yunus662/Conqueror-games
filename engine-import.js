abe3d8a39b615dc31da7b0a98546e93fd9976788
yunus662 <imyaay234@gmail.com>
2025-07-04 12:06:10 -0700

    Update engine-import.js

--- engine-import.js
+++ engine-import.js
@@ -29,7 +29,7 @@ export async function initializeGame() {
   // Establish Socket.IO connection.
   // Assumes that the Socket.IO script is loaded globally via CDN and available as 'io'.
   const socket = ("https://3db21b63-ee56-43d9-877f-e5b68b462df6-00-3s3v6c139aq4b.spock.replit.dev/");
-  socket.on("connect", () => {
+  socket("connect", () => {
     console.log(`Socket.IO: Connected as ${socket.id}`);
     socket.emit("join-lobby", lobby);
   });
